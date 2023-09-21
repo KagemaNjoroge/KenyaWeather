2ba7b31b6b7b52a3a0a4e7a213c9680e62250470
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-09-21 13:54:22 +0300

    Change site title and header

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,3 +3,6 @@ from .models import Weather
 
 # Register your models here.
 admin.site.register(Weather)
+# change the title of the admin site
+admin.site.site_header = 'Weather API Admin'
+admin.site.site_title = 'Weather API Admin'
