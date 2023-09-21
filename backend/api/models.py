aeaa8180421d330ee926b79b2b89ada4d5f855cc
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-09-21 13:49:51 +0300

    Weather model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Weather(models.Model):
+    city = models.CharField(max_length=100)
+    max_temp = models.CharField(max_length=100)
+    min_temp = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    city_id = models.IntegerField(primary_key=True)
+    date_time_fetched = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbose_name_plural = 'Weather'
