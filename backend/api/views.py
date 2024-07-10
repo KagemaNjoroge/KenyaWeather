82644fa96c952d1b0a72841d9f2a1dd5fb891f6c
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:28:47 +0300

    chore: Refactor all_weather view to improve code readability and maintainability

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+import stat
+from rest_framework.request import Request
 from .utils.scrapper import get_weather
-# json response
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from .serializers import WeatherSerializer
 from .models import Weather
 import time
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-@csrf_exempt
-def all_weather(request: HttpRequest) -> JsonResponse:
-    '''
-    Returns all weather data, currently only for Kenya.  
+@api_view(["GET"])
+def all_weather(request):
+    """
+    Returns all weather data, currently only for Kenya.
     Supported towns/cities are:
     - Nairobi
     - Mombasa
@@ -37,73 +37,51 @@ def all_weather(request: HttpRequest) -> JsonResponse:
     - Kisii
     - Machakos
 
-    '''
-    if request.method == 'GET':
-        #     # weather = get_weather()
+    """
 
-        #     # return JsonResponse(weather, safe=False)
-        # else:
-        #     # wrong request method
-        #     return HttpResponse(status=405, reason='Method Not Allowed')
+    # check if weather data in the database exceeds 3 hours
+    # if yes, fetch new data from the web, clear the database and save the new data
+    weather = Weather.objects.all()
 
-        # check if weather data in the database exceeds 3 hours
+    # current time - last fetch time
+    try:
+        time_diff_between_now_and_last_fetch = (
+            time.time() - weather[0].date_time_fetched.timestamp()
+        )
+    except:
+        time_diff_between_now_and_last_fetch = 10801
 
-        # if yes, fetch new data from the web, clear the database and save the new data
-        weather = Weather.objects.all()
-        if weather:
-            # current time - last fetch time
-            time_diff_between_now_and_last_fetch = time.time(
-            ) - weather[0].date_time_fetched.timestamp()
-            if time_diff_between_now_and_last_fetch > 10800:
-                # first try connecting to the web, if successful, clear the database and save the new data
-                try:
-                    weather = get_weather()
-                    for weather in weather:
-                        weather.delete()
-
-                    for weather_data in weather:
-                        w = Weather(
-                            city=weather_data['town_name'],
-                            max_temp=weather_data['max_temp'],
-                            min_temp=weather_data['min_temp'],
-                            description=weather_data['weather_description'],
-
-                        )
-                        w.save()
-                    return JsonResponse(weather, safe=False)
-                # if error connecting to web, return data in the database without clearing it
-                except:
-                    weather = Weather.objects.all()
-                    weather_data = []
-                    for w in weather:
-                        weather_data.append({
-                            'town_name': w.city,
-                            'max_temp': w.max_temp,
-                            'min_temp': w.min_temp,
-                            'weather_description': w.description,
-                        })
-                    return JsonResponse(weather_data, safe=False)
-            else:  # return data in the database
-
-                weather = Weather.objects.all()
-                weather_data = []
-                for w in weather:
-                    weather_data.append({
-                        'town_name': w.city,
-                        'max_temp': w.max_temp,
-                        'min_temp': w.min_temp,
-                        'weather_description': w.description,
-                    })
-                return JsonResponse(weather_data, safe=False)
-        else:
+    if time_diff_between_now_and_last_fetch > 10800:
+        # first try connecting to the web, if successful, clear the database and save the new data
+        try:
             weather = get_weather()
-            for weather_data in weather:
-                w = Weather(
-                    city=weather_data['town_name'],
-                    max_temp=weather_data['max_temp'],
-                    min_temp=weather_data['min_temp'],
-                    description=weather_data['weather_description'],
+            Weather.objects.all().delete()
+            for w in weather:
+                serializer = WeatherSerializer(data=w)
+                if serializer.is_valid():
+                    serializer.save()
+            weather = Weather.objects.all()
+            serializer = WeatherSerializer(weather, many=True)
+            return Response(serializer.data)
 
-                )
-                w.save()
-            return JsonResponse(weather, safe=False)
+        except:
+            # fall back to the database
+            weather = Weather.objects.all()
+            serializer = WeatherSerializer(weather, many=True)
+            return Response(serializer.data)
+    else:
+        # return data in the database
+        weather = Weather.objects.all()
+        serializer = WeatherSerializer(weather, many=True)
+        return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_weather_for_specific_city(request: Request):
+    query_city = request.query_params.get("city", None)
+    if query_city:
+        weather = Weather.objects.filter(city__iexact=query_city)
+        serializer = WeatherSerializer(weather, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"error": "Please provide a city name"}, status=400)
