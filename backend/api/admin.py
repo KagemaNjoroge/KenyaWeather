c2e088bcc572a69747dfa526e91cd6d58f2a036c
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:28:15 +0300

    chore: Update admin site header and title in /backend/api/admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Weather
 
-# Register your models here.
-admin.site.register(Weather)
-# change the title of the admin site
-admin.site.site_header = 'Weather API Admin'
-admin.site.site_title = 'Weather API Admin'
+admin.site.site_header = "Weather API Admin"
+admin.site.site_title = "Weather API Admin"
+
+
+@admin.register(Weather)
+class WeatherAdmin(admin.ModelAdmin):
+    list_display = ("city", "max_temp", "min_temp", "description", "date_time_fetched")
+    list_filter = ("city", "date_time_fetched")
+    search_fields = ("city", "description")
+    date_hierarchy = "date_time_fetched"
+    ordering = ("-date_time_fetched",)
+    readonly_fields = ("date_time_fetched",)
+    fieldsets = (
+        (None, {"fields": ("city", "max_temp", "min_temp", "description")}),
+        ("Date & Time", {"fields": ("date_time_fetched",), "classes": ("collapse",)}),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("city", "max_temp", "min_temp", "description")}),
+    )
