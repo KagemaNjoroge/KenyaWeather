17046109b42fe5c6aa9e31dfe5578e68f5f173be
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:28:25 +0300

    chore: Update verbose_name_plural for Weather model in models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,11 +8,10 @@ class Weather(models.Model):
     max_temp = models.CharField(max_length=100)
     min_temp = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    city_id = models.IntegerField(primary_key=True)
     date_time_fetched = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.city
 
     class Meta:
-        verbose_name_plural = 'Weather'
+        verbose_name_plural = "Weather"
