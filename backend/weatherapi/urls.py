93434df1ce94f99ac87dd6db64cc417847a8e1eb
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:29:09 +0300

    chore: Update Django version to 5.0.7 and add drf-yasg for API documentation

--- backend/weatherapi/urls.py
+++ backend/weatherapi/urls.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Weather API",
+        default_version="v1",
+        description="A simple weather API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="nyamburanjorogejames@students.uonbi.ac.ke"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path("admin/", admin.site.urls),
+    # swagger documentation
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("", include("api.urls")),
 ]
