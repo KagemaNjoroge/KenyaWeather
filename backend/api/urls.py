38951b41e76d6c4e3719e6902aff3a935c2de6ca
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:28:39 +0300

    chore: Add new API endpoint for getting weather data for a specific city

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import all_weather
-urlpatterns = [
-    path('all/', all_weather, name='all_weather')
+from .views import all_weather, get_weather_for_specific_city
 
+urlpatterns = [
+    path("all/", all_weather, name="all_weather"),
+    path(
+        "specific/", get_weather_for_specific_city, name="get_weather_for_specific_city"
+    ),
 ]
