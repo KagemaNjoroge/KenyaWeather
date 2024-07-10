ed5bca7e43ced9fc05b686bd2f10b0dc373732d2
Kagema Njoroge <reecejames934@gmail.com>
2024-07-11 01:28:32 +0300

    chore: Add WeatherSerializer for Weather model in serializers.py

---
+++ backend/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Weather
+
+
+class WeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weather
+        fields = "__all__"
+        read_only_fields = ["date_time_fetched"]
