0900fdee0aca7b3cdcc04f0556f7902adc95d449
Kagema Njoroge <reecejames934@gmail.com>
2024-12-11 16:36:13 +0300

    Remove unnecessary comments from models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Weather(models.Model):
     city = models.CharField(max_length=100)
     max_temp = models.CharField(max_length=100)
