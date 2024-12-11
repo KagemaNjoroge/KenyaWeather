bd7551a19f78bee78dda1b5a3beb55cdb7a3eac4
Kagema Njoroge <reecejames934@gmail.com>
2024-12-11 16:36:45 +0300

    Refactor weather API views to use class-based views and improve data fetching logic

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,22 @@
-import re
-import stat
-from rest_framework.request import Request
 from .utils.scrapper import get_weather
-from .serializers import WeatherSerializer
+from .serializers import SupportedTownsSerializer, WeatherSerializer
 from .models import Weather
 import time
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 
-@api_view(["GET"])
-def all_weather(request):
+class TownsList(ListAPIView):
+    """
+    Returns a list of all towns/cities supported by the API
+    """
+
+    serializer_class = SupportedTownsSerializer
+
+    def get_queryset(self):
+        return Weather.objects.all().values("city").distinct()
+
+
+class WeatherList(ListAPIView):
     """
     Returns all weather data, currently only for Kenya.
     Supported towns/cities are:
@@ -40,52 +46,51 @@ def all_weather(request):
 
     """
 
-    # check if weather data in the database exceeds 3 hours
-    # if yes, fetch new data from the web, clear the database and save the new data
-    weather = Weather.objects.all()
+    queryset = Weather.objects.all()
+    serializer_class = WeatherSerializer
 
-    # current time - last fetch time
-    try:
-        time_diff_between_now_and_last_fetch = (
-            time.time() - weather[0].date_time_fetched.timestamp()
-        )
-    except:
-        time_diff_between_now_and_last_fetch = 10801
+    # filtering support
+    filterset_fields = (
+        "city",
+        "max_temp",
+        "min_temp",
+        "description",
+        "date_time_fetched",
+    )
 
-    if time_diff_between_now_and_last_fetch > 10800:
-        # first try connecting to the web, if successful, clear the database and save the new data
-        try:
-            weather = get_weather()
-            Weather.objects.all().delete()
-            for w in weather:
-                serializer = WeatherSerializer(data=w)
-                if serializer.is_valid():
-                    serializer.save()
-            weather = Weather.objects.all()
-            serializer = WeatherSerializer(weather, many=True)
-            return Response(serializer.data)
+    def get_queryset(self):
+        """
+        check if weather data in the database exceeds 3 hours
+        if yes, fetch new data from the web, clear the database and save the new data
+        """
 
-        except:
-            # fall back to the database
-            weather = Weather.objects.all()
-            serializer = WeatherSerializer(weather, many=True)
-            return Response(serializer.data)
-    else:
-        # return data in the database
         weather = Weather.objects.all()
-        serializer = WeatherSerializer(weather, many=True)
-        return Response(serializer.data)
 
+        # current time - last fetch time
+        try:
+            time_diff_between_now_and_last_fetch = (
+                time.time() - weather[0].date_time_fetched.timestamp()
+            )
+        except:
+            time_diff_between_now_and_last_fetch = 10801
 
-@api_view(["GET"])
-def get_weather_for_specific_city(request: Request):
-    query_city = request.query_params.get("city", None)
-    if query_city:
-        weather = Weather.objects.filter(city=query_city)
-        if weather:
-            serializer = WeatherSerializer(weather.first())
-            return Response(serializer.data)
-        return Response({"error": "City not found"}, status=404)
+        if time_diff_between_now_and_last_fetch > 10800:
+            # first try connecting to the web, if successful, clear the database and save the new data
+            try:
+                weather = get_weather()
+                Weather.objects.all().delete()
+                for w in weather:
+                    serializer = WeatherSerializer(data=w)
+                    if serializer.is_valid():
+                        serializer.save()
+                weather = Weather.objects.all()
+                return weather
 
-    else:
-        return Response({"error": "Please provide a city name"}, status=400)
+            except:
+                # fall back to the database
+                weather = Weather.objects.all()
+                return weather
+        else:
+            # return data in the database
+            weather = Weather.objects.all()
+            return weather
