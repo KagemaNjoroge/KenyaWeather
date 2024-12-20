b9cb21e32287e022ef109017f2a928a8c57696a8
Kagema Njoroge <reecejames934@gmail.com>
2024-12-20 14:36:22 +0300

    Remove unused TownsList endpoint from API URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import WeatherList, TownsList
+from .views import WeatherList
 
 
 app_name = "api"
 
 urlpatterns = [
     path("weather/", WeatherList.as_view(), name="weather_list"),
-    path("towns/", TownsList.as_view(), name="towns_list"),
 ]
