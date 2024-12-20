546dd62985430baf39a81c80f705007ec22921d7
Kagema Njoroge <reecejames934@gmail.com>
2024-12-20 14:36:15 +0300

    Refactor Weather serializer by removing unused SupportedTownsSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import Weather
 
-from rest_framework.serializers import Serializer, CharField
-
-
-class SupportedTownsSerializer(Serializer):
-    city = CharField()
-
-    class Meta:
-        model = Weather
-        fields = ["city"]
-
 
 class WeatherSerializer(serializers.ModelSerializer):
     class Meta:
